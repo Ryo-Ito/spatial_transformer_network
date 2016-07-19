1908fb6f47c0c465fe7091d6969099e71abcbd4c
ryoito <r0735nj5058@gmail.com>
2016-07-19 17:31:07 +0900

    modified affine parameters

--- affine.py
+++ affine.py
@@ -3,7 +3,7 @@ from grid import batch_mgrid2d, batch_mgrid3d
 from warp import warp2d, warp3d
 
 
-def affine_warp2d(imgs, matrix, bias):
+def affine_warp2d(imgs, theta):
     """
     affine transforms 2d images
 
@@ -12,12 +12,9 @@ def affine_warp2d(imgs, matrix, bias):
     imgs : tf.Tensor
         images to be warped
         [n_batch, xlen, ylen, n_channel]
-    matrix : tf.Tensor
-        linear transformation
-        [n_batch, 2, 2]
-    bias : tf.Tensor
-        translation
-        [n_batch, 2]
+    theta : tf.Tensor
+        parameters of affine transformation
+        [n_batch, 6]
 
     Returns
     -------
@@ -28,19 +25,20 @@ def affine_warp2d(imgs, matrix, bias):
     n_batch = tf.shape(imgs)[0]
     xlen = tf.shape(imgs)[1]
     ylen = tf.shape(imgs)[2]
-    matrix = tf.to_float(matrix)
-    bias = tf.to_float(bias)
+    theta = tf.reshape(theta, [-1, 2, 3])
+    matrix = tf.slice(theta, [0, 0, 0], [-1, -1, 2])
+    t = tf.slice(theta, [0, 0, 2], [-1, -1, -1])
 
     grids = batch_mgrid2d(xlen, ylen, n_batch=n_batch)
     coords = tf.reshape(grids, [n_batch, 2, -1])
 
-    T_g = tf.batch_matmul(matrix, coords) + tf.expand_dims(bias, 2)
+    T_g = tf.batch_matmul(matrix, coords) + t
     T_g = tf.reshape(T_g, [n_batch, 2, xlen, ylen])
     output = warp2d(imgs, T_g)
     return output
 
 
-def affine_warp3d(imgs, matrix, bias):
+def affine_warp3d(imgs, theta):
     """
     affine transforms 3d images
 
@@ -49,12 +47,9 @@ def affine_warp3d(imgs, matrix, bias):
     imgs : tf.Tensor
         images to be warped
         [n_batch, xlen, ylen, zlen, n_channel]
-    matrix : tf.Tensor
-        linear transformation
-        [n_batch, 3, 3]
-    bias : tf.Tensor
-        translation
-        [n_batch, 3]
+    theta : tf.Tensor
+        parameters of affine transformation
+        [n_batch, 12]
 
     Returns
     -------
@@ -66,13 +61,14 @@ def affine_warp3d(imgs, matrix, bias):
     xlen = tf.shape(imgs)[1]
     ylen = tf.shape(imgs)[2]
     zlen = tf.shape(imgs)[3]
-    matrix = tf.to_float(matrix)
-    bias = tf.to_float(bias)
+    theta = tf.reshape(theta, [-1, 3, 4])
+    matrix = tf.slice(theta, [0, 0, 0], [-1, -1, 3])
+    t = tf.slice(theta, [0, 0, 3], [-1, -1, -1])
 
     grids = batch_mgrid3d(xlen, ylen, zlen, n_batch)
     grids = tf.reshape(grids, [n_batch, 3, -1])
 
-    T_g = tf.batch_matmul(matrix, grids) + tf.expand_dims(bias, 2)
+    T_g = tf.batch_matmul(matrix, grids) + t
     T_g = tf.reshape(T_g, [n_batch, 3, xlen, ylen, zlen])
     output = warp3d(imgs, T_g)
     return output
